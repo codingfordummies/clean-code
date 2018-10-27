c7ea0b218f62edc7a7cc8f5630e9fb0175379c92
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-27 20:44:10 +0200

    Update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../src"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
 
 # -- Import packages ---------------------------------------------------------
 
